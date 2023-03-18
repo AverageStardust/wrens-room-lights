84e865d93c289885e86ad0443acf9163f220dc3f
Wren Durbano <wren.durbano@gmail.com>
2023-03-17 20:48:42 -0600

    fixed deno permissions error

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import { log } from "./helper.ts";
 
 function init() {
-	const controlProcess = Deno.run({ cmd: ["deno", "run", "--allow-net", "--allow-read", "--allow-write", "--allow-run", "main.ts"] });
+	const controlProcess = Deno.run({ cmd: ["deno", "run", "--allow-net", "--allow-read", "--allow-write", "--allow-run", "--allow-env", "main.ts"] });
 	log("control process started");
 
 	controlProcess.status().then((status) => {
